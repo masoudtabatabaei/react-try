3b2d121e68cb1abd1e6455d8def9178f88a7d3fb
Masoud <masoudtabatabaei20@gmail.com>
2021-12-18 13:35:20 +0330

    use from useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import './App.css';
 import Todo from './Todo';
 
 function App() {
   const [todos , setTodos] = useState(['Todo1' , 'Todo2']);
   const [counter , setCounter] = useState(0);
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      setCounter(c => c + 1);
+    } , 1000);
+
+    // console.log(timeout);
+
+    return () => clearTimeout(timeout);
+  });
 
   const increment = () => {
     setCounter(c => c + 1);
@@ -17,8 +26,8 @@ function App() {
     <div className='container'>
       <div>counter : {counter}</div>
       <hr/>
-      <Todo todos={todos}/>
       <button onClick={increment}> + </button>
+      <Todo todos={todos}/>
     </div>
   );
 }
