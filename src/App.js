dcd99b7544e651c8c47cdeec15a749176c0e5554
Masoud <masoudtabatabaei20@gmail.com>
2021-12-20 23:55:20 +0330

    implement lifting state up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import { useState , useEffect, useRef } from "react";
 import './App.css';
-import Todo from './Todo';
+import Calculator from "./Calculator";
 
 function App() {
-  const [todos , setTodos] = useState(['Todo1' , 'Todo2']);
-  const inputElem = useRef();
-
-  const increment = () => {
-    let input = inputElem.current;
-    setTodos(prevTodos => [...prevTodos , input.value]);
-    input.value = "";
-    input.focus();
-  }
-
-  const handleInput = (e) => {
-    inputElem.current.value = e.target.value;
-  };
-
   return (
-    <div className='container'>
-      {/* <div>counter : {count.current}</div> */}
-      <hr/>
-      <input type='text' ref={inputElem} onChange={handleInput}/>
-      <br/>
-      <button onClick={increment}> + </button>
-      <Todo todos={todos}/>
+    <div className="container">
+      <Calculator/>
     </div>
   );
 }
